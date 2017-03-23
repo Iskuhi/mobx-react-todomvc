4edbd7d4d24828030415e5ec0444ee77416edfbd
Ani Hambardzumyan <anihambardzumyan@workfront.com>
2017-03-23 11:18:40 +0400

    mock data

---
+++ src/models/mockData.js
@@ -0,0 +1,150 @@
+var leftSide = {
+    ProjIDbgdfgt474756rgfhy574: {
+        name: "ProjName",
+        nodes: {
+            RoleID1: {
+                name: "RoleName1",
+                nodes: {
+                    userID1: {
+                        name: "UserName1",
+                        nodes: {
+                            TaskID1: {
+                                name: "TaskName",
+                                nodes: {}
+                            }
+                        }
+                    },
+                    userID2: {
+                        name: "UserName2",
+                        nodes: {
+                            TaskID2: {
+                                name: "TaskName",
+                                nodes: {}
+                            }
+                        }
+                    }
+                }
+            },
+            RoleID2: {
+                name: "RoleName2"
+                nodes: {
+                    userID1: {
+                        name: "UserName1",
+                        nodes: {
+                            TaskID1: {
+                                name: "TaskName",
+                                nodes: {}
+                            }
+                        }
+                    },
+                    userID2: {
+                        name: "UserName2",
+                        nodes: {
+                            TaskID2: {
+                                name: "TaskName",
+                                nodes: {}
+                            }
+                        }
+                    }
+                }
+            }
+        },
+
+        ProjID2: {
+            name: "ProjName",
+            nodes: {
+                RoleID1: {
+                    name: "RoleName1",
+                    nodes: {
+                        userID1: {
+                            name: "UserName1",
+                            nodes: {
+                                TaskID1: {
+                                    name: "TaskName",
+                                    nodes: {}
+                                }
+                            }
+                        },
+                        userID2: {
+                            name: "UserName2",
+                            nodes: {
+                                TaskID2: {
+                                    name: "TaskName",
+                                    nodes: {}
+                                }
+                            }
+                        }
+                    }
+                },
+                RoleID2: {
+                    name: "RoleName2",
+                    nodes: {
+                        userID1: {
+                            name: "UserName1",
+                            nodes: {
+                                TaskID1: {
+                                    name: "TaskName",
+                                    nodes: {}
+                                }
+                            }
+                        },
+                        userID2: {
+                            name: "UserName2",
+                            nodes: {
+                                TaskID2: {
+                                    name: "TaskName",
+                                    nodes: {}
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+
+
+
+var rightSide = {
+    date1: {
+        avl: {
+            PROJID: {
+                avl: null,
+                nodes: {
+                    RoleID1: {
+                        avl: null,
+                        nodes: {
+                            UserId: {
+                                avl: 10,
+                                nodes: {
+                                    TaskId: {
+                                        avl: null,
+                                        nodes: {}
+                                    }
+                                }
+                            }
+                        }
+                    } ,
+                    RoleID2: {
+                        avl: null,
+                        nodes: {
+                            UserId: {
+                                avl: 10,
+                                nodes: {
+                                    TaskId: {
+                                        avl: null,
+                                        nodes: {}
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        bdg: {},
+        pln: {},
+        act: {},
+    }
+};
